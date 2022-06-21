046f6b122f18eabeac1807456aef7b25ded2e158
Peter Kimani <peterjajakim@gmail.com>
2022-06-21 12:10:19 +0300

    FETCHED DATA FROM BOOK API

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  fetch("https://book4.p.rapidapi.com/?rapidapi-key=07fa193556msh889d95d2b765e6cp133e5cjsna64dd92f0841")
+	.then(parseResponse)
+	.then(logResponse)
+
+function parseResponse(response){
+	return response.json()
+}
+
+function logResponse(response){
+	console.log(response)
+}
